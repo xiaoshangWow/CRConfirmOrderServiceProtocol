d36fb79f8391169631f0c8247c14d03c4c56f46b
许磊 <lxu@jinhetech.com>
2018-02-11 17:52:51 +0800

    02.11.1

--- CRConfirmOrderServiceProtocol/CRConfirmOrderServiceProtocol/CRConfirmOrderServiceProtocol.h
+++ CRConfirmOrderServiceProtocol/CRConfirmOrderServiceProtocol/CRConfirmOrderServiceProtocol.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@protocol CRConfirmOrderServiceProtocol <NSObject>
+@protocol CRConfirmOrderServiceProtocol  <NSObject>
 
 - (UIViewController *)confirmOrderViewControllerWithGoodsId:(NSString *)goodsId sureComplete:(dispatch_block_t)sureComplete;
 
